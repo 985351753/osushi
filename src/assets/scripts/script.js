2cf4ae4ff1fde45a96125c59a4615a1fb02a4400
985351753 <shizubiz32@gmail.com>
2025-10-10 23:14:58 +0900

    2025-10-10 pm2314

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,176 @@
+'use strict';
+
+$(document).ready(function(){
+    $('.hero__item')
+    // .on("init", function () {
+    //     $('.slick-slide[data-slick-index="0"]').addClass("add-animation");
+    //   })
+      .slick(
+        {
+        autoplay: true,
+        fade: true,
+        arrows: false,
+        speed: 500,
+        autoplaySpeed: 1000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        touchMove: false,
+        waitForAnimate: false,
+      }
+    );
+    //   .on({
+    //     beforeChange: function (event, slick, currentSlide, nextSlide) {
+    //       $(".slick-slide", this).eq(nextSlide).addClass("add-animation");
+    //       $(".slick-slide", this).eq(currentSlide).addClass("remove-animation");
+    //     },
+    //     afterChange: function () {
+    //       $(".remove-animation", this).removeClass(
+    //         "remove-animation add-animation"
+    //       );
+    //     },
+    // });
+
+});//slick
+
+const ham = $('.hamburger');
+const nav = $('.wrapper');
+var state = false;
+var scrollpos;
+ham.on('click', function () {
+  ham.toggleClass('active');
+  nav.toggleClass('active');
+  if(state == false) {
+    scrollpos = $(window).scrollTop();
+    $('body').addClass('fixed').css({'top': -scrollpos});
+    state = true;
+  } else {
+    $('body').removeClass('fixed').css({'top': 0});
+    $(window).scrollTop(scrollpos);
+    state = false;
+  }
+});//スクロール固定
+
+const a = $('.footerlink a');
+a.on ('click', function() {
+ham.toggleClass('active');
+nav.toggleClass('active');
+$('body').removeClass('fixed').css({'top': 0});
+$(window).scrollTop(scrollpos);
+state = false;
+});/* 子ページfooter飛ばし */
+
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');//スムーズスクロール
+    return false;
+  });
+});
+
+  function fadeAnime(){
+
+  $('.fadeUpTrigger').each(function(){
+    var elemPos = $(this).offset().top-0;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    }
+  });//.fadeUpTrigger
+
+  $('.fadeRightTrigger').each(function(){
+    var elemPos = $(this).offset().top-0;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeRight');
+    }
+  });//.fadeRightTrigger
+
+  $('.blurTrigger').each(function(){
+    var elemPos = $(this).offset().top-0;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('blur');
+    }
+  });//.blurTrigger
+}//function fadeAnime
+
+$(window).scroll(function (){
+  fadeAnime();
+});
+
+$(window).on('load', function () {
+  fadeAnime();
+});
+
+gsap.utils.toArray(".title").forEach((title) => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: title,  // 各タイトルをトリガーにする
+      start: "top 80%", 
+      markers: false
+    }
+  });
+
+  gsap.utils.toArray(title.querySelectorAll(".appears")).forEach((el, i) => {
+  tl.from(el, {
+    opacity: 0,
+    y: 40,
+    duration: 1,
+    ease: "power3.out",
+    delay: i * 0.2   // 0.3 秒ずつずらす
+  }, "<"); // "<" で前のアニメ直後からスタート
+  });
+  
+  tl.from(title.querySelector(".text"), { opacity: 0, y: 40, duration: 1, ease: "power3.out" })
+    .from(title.querySelector(".thumb"), { opacity: 0, y: 40, duration: 1 }, "-=0.8")
+    .from(title.querySelector(".button"), { opacity: 0, y: 40, duration: 0.5 }, "-=1");
+});
+
+
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 300){
+    $('.appBtn').removeClass('RightMove');
+    $('.appBtn').addClass('LeftMove');
+  }else{
+    if(
+      $('.appBtn').hasClass('LeftMove')){
+      $('.appBtn').removeClass('LeftMove');
+      $('.appBtn').addClass('RightMove');
+    }
+  }
+}
+
+$(window).scroll(function () {
+  PageTopAnime();
+});
+
+$(window).on('load', function () {
+  PageTopAnime();
+});
+
+
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0 }, 500);
+  return false;
+});/* totop */
+
+
+let ua = navigator.userAgent;
+if(ua.indexOf("iPhone") === -1 && ua.indexOf("Android") === -1) {
+  jQuery('a[href^="tel:"]')
+    .css("cursor", "default")
+    .on("click", function(e) {
+      e.preverntDefault();
+    });
+}/* tel */
